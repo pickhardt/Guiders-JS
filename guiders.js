ae0d90ac18c0f95d99d0cf4e9dcf5055a4fc2d3b
Stephen Farmer <sfarmer@majorcode.com>
2014-02-18 23:35:24 -0500

    Since we de-highlight guider.highlight, we should be highlighting it instead of guider.attachTo.
    While it is common to set highlight to the same ID as attachTo, I often attach to one thing but highlight another--like the attachTo's container. Also, must the guider have an attachTo for a highlight to be valid? If not, the test of attachTo is also unnecessary. It's no harm right now, so I left it in.

--- guiders.js
+++ guiders.js
@@ -596,7 +596,7 @@ var guiders = (function($) {
       guiders._showOverlay(myGuider);
       // if guider is attached to an element, make sure it's visible
       if (myGuider.highlight && myGuider.attachTo) {
-        guiders._highlightElement(myGuider.attachTo);
+        guiders._highlightElement(myGuider.highlight);
       }
     }
     
